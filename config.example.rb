b33169ea2fbc586c09157e1340f5c9f1b4d2cb9c
Magnus Holm <judofyr@gmail.com>
2019-05-12 13:34:27 +0200

    Add README

---
+++ config.example.rb
@@ -0,0 +1,5 @@
+# This must match up with your PostgreSQL credentials:
+set :database_url, 'postgres:///rfp'
+
+# This must just be a random string:
+set :secret, "1f28c64dec80e03e354f3a742d69884bc390118ea449c0084a278c4566779ed5"
