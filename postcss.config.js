aa9b4963d5b472e824ba45dc9e439e37fb9534f7
Magnus Holm <judofyr@gmail.com>
2019-05-15 13:42:13 +0200

    Optimize CSS with PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,18 @@
-module.exports = ({env}) => {
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './app/**/*.rb',
+  ],
 
+  // We need to include : and / which Tailwind uses
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = ({env,}) => {
   return {
     plugins: [
       require("tailwindcss"),
-      env === 'production' && require("cssnano")
+      env === 'production' && purgecss,
+      env === 'production' && require("cssnano"),
     ]
   };
 };
